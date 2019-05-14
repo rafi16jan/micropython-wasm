001c3f1962a68fe5a9a42c6a61ac581bc6de4e04
Farrell Rafi <farrell@odooabc.com>
2019-05-14 10:07:41 +0700

    Add webpack/browser support

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 var mp = require('./lib/micropython.js');
-mp.onRuntimeInitialized();
+var browser = true;
+if (typeof window === 'undefined' && typeof importScripts === 'undefined') {
+  mp.onRuntimeInitialized();
+  browser = false;
+}
 module.exports.init = global.mp_js_init;
 module.exports.do_str = global.mp_js_do_str;
 module.exports.init_repl = global.mp_js_init_repl;
 module.exports.process_char = global.mp_js_process_char;
+if (browser) {
+  var stdout = document.createElement('div');
+  stdout.id = 'mp_js_stdout';
+  stdout.style.display = 'none';
+  stdout.addEventListener('print', function (event) {
+    console.log(event.data);
+  }, false);
+  document.body.append(stdout);
+  var interval = setInterval(() => {
+    if (global.mp_js_init) {
+      module.exports.init = global.mp_js_init;
+      module.exports.do_str = global.mp_js_do_str;
+      module.exports.init_repl = global.mp_js_init_repl;
+      module.exports.process_char = global.mp_js_process_char;
+      clearInterval(interval);
+    }
+  }, 0);
+}
